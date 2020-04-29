f3090a762ec0201c32647bbc6d44566f81cec2c0
person <agdehaan@gmail.com>
2020-04-29 12:35:01 +0200

    Added reload command

---
+++ src/vepnar/bettermobs/commands/ReloadCommand.java
@@ -0,0 +1,36 @@
+package vepnar.bettermobs.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import vepnar.bettermobs.Main;
+
+public class ReloadCommand implements CommandExecutor {
+	
+	Main javaplugin;
+	
+	/**
+	 * Initialize the Bettermobs reload command listener
+	 * 
+	 * @param m the JavaPlugin class or main file
+	 */
+	public ReloadCommand(Main m) {
+		javaplugin = m;
+	}
+
+	/**
+	 * Handle the /bettermobs-reload and /bmr command.
+	 * Reload config, create new config and reload events.
+	 * Will also send a message when the reload has completed.
+	 */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+    	javaplugin.reloadConfig();
+    	javaplugin.loadDefaultConfig();
+        javaplugin.enableEvents();
+        sender.sendMessage(javaplugin.prefix + "has been reloaded");
+        return true;
+    }
+}
