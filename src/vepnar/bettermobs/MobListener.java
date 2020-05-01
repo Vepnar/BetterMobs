ea357afca4001f44787cd18d9ff25b9e6d5ad397
person <agdehaan@gmail.com>
2020-05-01 12:50:02 +0200

    Improve Moblistener

--- src/vepnar/bettermobs/MobListener.java
+++ src/vepnar/bettermobs/MobListener.java
@@ -9,6 +9,7 @@ package vepnar.bettermobs;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EnderDragonChangePhaseEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 // Events
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -27,51 +28,77 @@ public class MobListener implements Listener {
 
 		javaplugin = m;
 	}
-	
+
 	/**
-	 * Handle all PlayerMoveEvent's. and call events who are enabled in the configuration file.
-	 * Only do this rarely because this is quite CPU heavy when there are a lot of active players.
+	 * Handle all PlayerMoveEvent's. and call events who are enabled in the
+	 * configuration file. Only do this rarely because this is quite CPU heavy when
+	 * there are a lot of active players.
 	 */
 	@EventHandler
 	public void onEntityInteractEvent(PlayerMoveEvent e) {
 		int rand = (int) (Math.random() * tick); // Reduce lag
-		if(rand != 1 || javaplugin.listen) return;
-		
+		if (rand != 1 || javaplugin.listen)
+			return;
+
 		for (EventClass event : javaplugin.eventList) {
+			if (e.isCancelled())
+				break;
 			if (!event.canBeCalled(e))
 				continue;
 			event.callEvent(e);
 		}
 		tick = javaplugin.getConfig().getInt("entityUpdateSpeed");
 	}
-	
+
 	/**
-	 * Handle all entity spawn events.
-	 * Don't call this function yourself!
+	 * Handle all entity spawn events. Don't call this function yourself!
 	 */
 	@EventHandler
 	public void onEntitySpawn(CreatureSpawnEvent e) {
-		if(javaplugin.listen) return;
-		
+		if (javaplugin.listen)
+			return;
+
 		for (EventClass event : javaplugin.eventList) {
-			if (!event.canBeCalled(e)) continue;
+			if (e.isCancelled())
+				break;
+			if (!event.canBeCalled(e))
+				continue;
 			event.callEvent(e);
 		}
 	}
+
 	/**
-	 * Handle all projectile launch events.
-	 * Don't call this function yourself!
+	 * Handle all projectile launch events. Don't call this function yourself!
 	 */
 	@EventHandler
 	public void onProjectileLaunch(ProjectileLaunchEvent e) {
-		if(javaplugin.listen) return;
-		
+		if (javaplugin.listen)
+			return;
+
 		for (EventClass event : javaplugin.eventList) {
-			if (!event.canBeCalled(e)) continue;
+			if (e.isCancelled())
+				break;
+			if (!event.canBeCalled(e))
+				continue;
 			event.callEvent(e);
 		}
 	}
-	
-	
+
+	/**
+	 * Handle all dragon phases events. Don't call this function yourself!
+	 */
+	@EventHandler
+	public void onDragonChangePhase(EnderDragonChangePhaseEvent e) {
+		if (javaplugin.listen)
+			return;
+
+		for (EventClass event : javaplugin.eventList) {
+			if (e.isCancelled())
+				break;
+			if (!event.canBeCalled(e))
+				continue;
+			event.callEvent(e);
+		}
+	}
 
 }
