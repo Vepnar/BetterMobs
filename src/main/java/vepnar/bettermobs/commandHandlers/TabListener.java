42e484709c56f01d163e0b129d9e8d24f0a5a1d5
Vepnar <agdehaan@gmail.com>
2021-12-14 16:56:48 +0100

    Implement basic tab completion

--- src/main/java/vepnar/bettermobs/commandHandlers/TabListener.java
+++ src/main/java/vepnar/bettermobs/commandHandlers/TabListener.java
@@ -1,12 +1,18 @@
 package vepnar.bettermobs.commandHandlers;
 
+import com.sun.istack.internal.NotNull;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import vepnar.bettermobs.Main;
+import vepnar.bettermobs.genericMobs.IMobListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The current implementation doesn't support multi-argument commands.
+ */
 public class TabListener implements TabCompleter {
 
     Main core;
@@ -17,8 +23,72 @@ public class TabListener implements TabCompleter {
         this.bettermobs = bettermobs;
     }
 
+    private List<String> getSubCommands(CommandSender sender, ICommandGroup command) {
+        ArrayList<String> tabs = new ArrayList<>();
+        for (ICommand subCommand : command.getCommands()) {
+
+            // Only show subcommands the sender has access to.
+            if (!CommandUtils.hasPermissions(sender, command)) continue;
+
+            tabs.add(subCommand.getName());
+        }
+        return tabs;
+    }
+
+    private List<String> getModules() {
+        ArrayList<String> names = new ArrayList<>();
+        for (IMobListener listener : core.mobListeners) {
+            names.add(listener.getName());
+        }
+        return names;
+    }
+
+    private List<String> getCompletionList(CommandSender sender, ICommand command) {
+        switch (command.TabType()) {
+            case SUBCOMMAND:
+                if (command instanceof ICommandGroup) {
+                    return getSubCommands(sender, (ICommandGroup) command);
+                } else {
+                    core.getLogger().warning("Error trying to expand tab of " + command.getName());
+                    return null;
+                }
+            case MODULE:
+                return getModules();
+            case NOTHING:
+                return new ArrayList<>();
+            default:
+                return null;
+
+        }
+    }
+
+    private ICommand getSubCommand(ICommand command, CommandSender sender, String targetString) {
+        // Only traverse command groups.
+        if (!(command instanceof ICommandGroup)) return null;
+        ICommandGroup group = (ICommandGroup) command;
+
+        // Return a subcommand if found.
+        for (ICommand child : group.getCommands()) {
+            if (child.getName().equalsIgnoreCase(targetString)) {
+
+                // Verify if the sender has enough permissions
+                if (CommandUtils.hasPermissions(sender, child)) {
+                    return child;
+                } else return null;
+            }
+        }
+        return null;
+    }
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return null;
+        ICommand deepestCommand = bettermobs;
+        for (String target : args) {
+            ICommand matching = getSubCommand(deepestCommand, sender, target);
+            if (matching != null) {
+                deepestCommand = matching;
+            } else break;
+        }
+        return getCompletionList(sender, deepestCommand);
     }
 }
