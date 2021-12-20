64caa2340026b9e97ac963b5a37d78ef3c5fbbcc
Vepnar <agdehaan@gmail.com>
2021-12-20 17:14:57 +0100

    Resource move improvement thanks to fulcanelly

--- src/main/java/vepnar/bettermobs/genericMobs/GenericMob.java
+++ src/main/java/vepnar/bettermobs/genericMobs/GenericMob.java
@@ -1,13 +1,9 @@
 package vepnar.bettermobs.genericMobs;
 
-import com.google.common.io.ByteStreams;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class GenericMob implements IMobListener {
 
@@ -37,11 +33,7 @@ public class GenericMob implements IMobListener {
         // Attempt to create a configuration file.
         try {
             if (!configFile.exists()) {
-                configFile.createNewFile();
-                try (InputStream in = core.getResource("mobs/" + getName() + ".yml");
-                     OutputStream out = new FileOutputStream(configFile)) {
-                    ByteStreams.copy(in, out);
-                }
+                core.saveResource("mobs/" + getName() + ".yml", false);
                 core.getLogger().info(getName() + " config has been loaded.");
             }
         } catch (Exception e) {
